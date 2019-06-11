cc85141c51662bc10c46dc57f421e005a81a55b0
Gustavo Leme <gleme@nevada.unr.edu>
2019-06-11 19:40:46 -0300

    config to antd css

--- app/next.config.js
+++ app/next.config.js
@@ -1,23 +1,12 @@
 /* eslint-disable */
-const withLess = require('@zeit/next-less');
-const lessToJS = require('less-vars-to-js');
-const fs = require('fs');
-const path = require('path');
+const withCss = require('@zeit/next-css');
 
-// Where your antd-custom.less file lives
-const themeVariables = lessToJS(
-  fs.readFileSync(
-    path.resolve(__dirname, './node_modules/antd/dist/antd.less'),
-    'utf8'
-  )
-);
-// fix: prevents error when .less files are required by node
 if (typeof require !== 'undefined') {
-  require.extensions['.less'] = file => {};
+  require.extensions['.css'] = file => {};
 }
 
 module.exports = () => {
-  return withLess({
+  return withCss({
     webpackDevMiddleware: config => {
       // Solve compiling problem via vagrant
       config.watchOptions = {
@@ -25,11 +14,6 @@ module.exports = () => {
         aggregateTimeout: 300 // delay before rebuilding
       };
       return config;
-    },
-    lessLoaderOptions: {
-      javascriptEnabled: true,
-      ignoreOrder: true,
-      modifyVars: themeVariables // make your antd custom effective
     }
   });
 };
