cc521358fc31d17e8ab2aca24b6b55bbd03e8f47
Gustavo Leme <gleme@nevada.unr.edu>
2019-06-11 19:41:15 -0300

    add dynamic menu generation by pages

---
+++ app/components/layout/pages.js
@@ -0,0 +1,29 @@
+const pages = [
+  {
+    label: 'Home',
+    path: '/',
+    icon: 'home'
+  },
+  {
+    label: 'Patient',
+    path: '/patient',
+    icon: 'user'
+  },
+  {
+    label: 'Consultation',
+    path: '/consultation',
+    icon: 'reconciliation'
+  },
+  {
+    label: 'Screening',
+    path: '/screening',
+    icon: 'exception'
+  },
+  {
+    label: 'Diagnosis',
+    path: '/diagnosis',
+    icon: 'monitor'
+  }
+];
+
+export default pages;
