b76004e86f8ab4f80e50e195a61a494b6e3f0b56
Gustavo Leme <gleme@nevada.unr.edu>
2019-06-13 19:52:38 -0300

    add screening functionalities

--- app/components/screening/Form.js
+++ app/components/screening/Form.js
@@ -1,99 +1,119 @@
-import { Form, Button, DatePicker, Checkbox, Col, Row, Typography, Input } from 'antd';
+import { Component, Fragment } from 'react';
+import { Form, DatePicker, Checkbox, Typography, Input, Modal } from 'antd';
+import produce from 'immer';
 import moment from 'moment';
-const { Paragraph } = Typography;
-import questions from '../../static/questions';
+import { set } from 'lodash';
+const { Text } = Typography;
 
-class ScreeningForm extends React.Component {
+import patientService from '../../services/patient';
+import screeningService from '../../services/screening';
+
+function hasErrors(fieldsErrors) {
+  return Object.keys(fieldsErrors).some(field => fieldsErrors[field]);
+}
+
+class ScreeningForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      confirmDirty: false
+      patientCpf: {
+        validateStatus: ''
+      }
     };
   }
 
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.form.validateFieldsAndScroll((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-  };
+  componentDidMount() {
+    // To disabled submit button at the beginning.
+    this.props.form.validateFields();
+  }
 
-  handleConfirmBlur = e => {
-    const value = e.target.value;
-    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  handleValidateCPF = async (rule, value, callback) => {
+    if (value) {
+      this.setState(
+        produce(draft => {
+          set(draft, 'patientCpf.validateStatus', 'validating');
+        })
+      );
+      try {
+        await patientService.exists(value);
+        this.setState(
+          produce(draft => {
+            set(draft, 'patientCpf.validateStatus', 'success');
+          })
+        );
+        callback();
+      } catch (error) {
+        this.setState(
+          produce(draft => {
+            set(draft, 'patientCpf.validateStatus', 'error');
+          })
+        );
+        console.error('error', error);
+        callback('error to validate CPF!');
+      }
+    }
   };
 
   render() {
-    const { getFieldDecorator } = this.props.form;
-
+    const { form, visible, editMode, onOk, onCancel } = this.props;
+    const { patientCpf } = this.state;
+    const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = form;
+    const title = (editMode ? 'Edit ' : 'New ') + 'Screening';
+    const buttonText = editMode ? 'Update' : 'Create';
+    const patientCpfError = (isFieldTouched('patientCpf') && getFieldError('patientCpf')) || '';
     const formItemLayout = {
       labelCol: {
         xs: { span: 24 },
-        sm: { span: 8 }
+        sm: { span: 6 }
       },
       wrapperCol: {
         xs: { span: 24 },
-        sm: { span: 16 }
+        sm: { span: 18 }
       }
     };
-    const tailFormItemLayout = {
-      wrapperCol: {
-        xs: {
-          span: 24,
-          offset: 0
-        },
-        sm: {
-          span: 16,
-          offset: 8
-        }
-      }
-    };
-    const splitIndex = Math.ceil(questions.length / 2);
-    const fstColQuestions = questions.filter((question, index) => index < splitIndex);
-    const secColQuestions = questions.filter((question, index) => index >= splitIndex);
-    return (
-      <Form {...formItemLayout} onSubmit={this.handleSubmit}>
-        <Form.Item label="Patient CPF">
-          {/* intialValue: 42668869854, */}
-          {getFieldDecorator('patient', {
-            rules: [{ required: true, message: 'Please input a CPF!' }]
-          })(<Input />)}
-        </Form.Item>
 
-        <Col span={12} {...tailFormItemLayout}>
-          {fstColQuestions.map(({ subtitle, title, inputName }, index) => (
-            <Row key={index}>
-              <Form.Item key={inputName}>
-                {getFieldDecorator(inputName)(<Checkbox>{title}</Checkbox>)}
-                <Paragraph>{subtitle}</Paragraph>
+    return (
+      <Modal
+        visible={visible}
+        title={title}
+        okText={buttonText}
+        okButtonProps={{ disabled: hasErrors(getFieldsError()) }}
+        onOk={onOk}
+        onCancel={onCancel}
+      >
+        <Form {...formItemLayout} onSubmit={this.handleSubmit} styles={{ width: '100%' }}>
+          <Form.Item
+            label="Patient CPF"
+            hasFeedback
+            validateStatus={patientCpf.validateStatus}
+            help={patientCpfError}
+          >
+            {getFieldDecorator('patientCpf', {
+              initialValue: '',
+              validateTrigger: 'onBlur',
+              rules: [
+                { required: true, message: 'Please input a patient CPF!' },
+                { validator: this.handleValidateCPF }
+              ]
+            })(<Input />)}
+          </Form.Item>
+          {screeningService.questions.map(({ subtitle, title, inputName }, index) => (
+            <Fragment key={inputName}>
+              <Form.Item style={{ marginBottom: 0 }}>
+                {getFieldDecorator(inputName, { initialValue: false })(
+                  <Checkbox>{title}</Checkbox>
+                )}
               </Form.Item>
-            </Row>
+              <Text style={{ marginBottom: 10 }}>{subtitle}</Text>
+            </Fragment>
           ))}
-        </Col>
-        <Col span={12}>
-          {secColQuestions.map(({ subtitle, title, inputName }, index) => (
-            <Row key={index}>
-              <Form.Item key={inputName}>
-                {getFieldDecorator(inputName)(<Checkbox>{title}</Checkbox>)}
-                <Paragraph>{subtitle}</Paragraph>
-              </Form.Item>
-            </Row>
-          ))}
-        </Col>
-        <Form.Item />
-        <Form.Item label="Date">
-          {getFieldDecorator('date', {
-            initialValue: moment()
-          })(<DatePicker />)}
-        </Form.Item>
-        <Form.Item {...tailFormItemLayout}>
-          <Button type="primary" htmlType="submit">
-            Register
-          </Button>
-        </Form.Item>
-      </Form>
+          <Form.Item label="Date">
+            {getFieldDecorator('date', {
+              initialValue: moment()
+            })(<DatePicker />)}
+          </Form.Item>
+        </Form>
+      </Modal>
     );
   }
 }
--- app/services/screening.js
+++ app/services/screening.js
@@ -1,17 +1,138 @@
+import async from 'async';
+import { Promise } from 'bluebird';
+import { pickBy, identity } from 'lodash';
 import axios from './axios';
 
-export async function findQuestions() {
+Promise.promisifyAll(async);
+
+export async function find(params) {
   try {
-    const { data } = await axios.get('');
+    const { data } = await axios.get('/screening', { params });
     return data;
   } catch (error) {
     throw error;
   }
 }
 
-export async function createScreening(screening) {
+export async function create(patient, date, flags) {
   try {
+    const screening = {
+      patient,
+      date,
+      ...flags
+    };
     const { data } = await axios.post('/screening', screening);
-    return data;
-  } catch (error) {}
+    return {
+      ...data,
+      ...screening
+    };
+  } catch (error) {
+    throw error;
+  }
 }
+
+export async function findAll(expand) {
+  try {
+    const params = pickBy(
+      {
+        expand: expand ? `${expand.join(',')}` : undefined
+      },
+      identity
+    );
+    const { data } = await axios.get('/screening', { params });
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function remove(id) {
+  try {
+    const response = await axios.delete(`/screening/${id}`);
+    return response.status === 204;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function deleteAll(ids) {
+  try {
+    await async.each(ids, async (id, callback) => {
+      remove(id)
+        .then(() => callback())
+        .catch(error => callback(error));
+    });
+    return true;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export const questions = Object.freeze([
+  {
+    title: 'Manchas Avermelhadas',
+    subtitle:
+      'O paciente está apresentando, ou se queixando de manchas avermelhadas na pele (exantema maculopapular eritematoso), que começam no rosto e progridem em direção aos pés?',
+    inputName: 'isReddish'
+  },
+  {
+    title: 'Febre',
+    subtitle: 'O paciente está apresentando, ou se queixando de febre?',
+    inputName: 'isFever'
+  },
+  {
+    title: 'Tosse',
+    subtitle: 'O paciente está apresentando, ou se queixando de tosse?',
+    inputName: 'isCough'
+  },
+  {
+    title: 'Mal-estar',
+    subtitle: 'O paciente está apresentando, ou se queixando de mal-estar?',
+    inputName: 'isMalaise'
+  },
+  {
+    title: 'Conjuntivite',
+    subtitle: 'O paciente está apresentando, ou se queixando de conjuntivite?',
+    inputName: 'isConjuctivitis'
+  },
+  {
+    title: 'Coriza',
+    subtitle: 'O paciente está apresentando, ou se queixando de coriza?',
+    inputName: 'isCoryza'
+  },
+  {
+    title: 'Perda do apetite',
+    subtitle: 'O paciente está apresentando, ou se queixando de perda do apetite?',
+    inputName: 'isLossAppetite'
+  },
+  {
+    title: 'Manchas brancas',
+    subtitle:
+      'O paciente está apresentando, ou se queixando de manchas brancas na parte interna das bochechas (exantema de Koplik)?',
+    inputName: 'isWhiteSpots'
+  },
+  {
+    title: 'Diarreia',
+    subtitle: 'O paciente está apresentando, ou se queixando de Diarreia?',
+    inputName: 'isDiarrehea'
+  },
+  {
+    title: 'Convulsões',
+    subtitle: 'O paciente está apresentando, ou se queixando de Convulsões?',
+    inputName: 'isConvulsion'
+  },
+  {
+    title: 'Infecção no ouvido',
+    subtitle: 'O paciente está apresentando, ou se queixando de Infecção no ouvido?',
+    inputName: 'isEarInfection'
+  }
+]);
+
+export default {
+  find,
+  findAll,
+  create,
+  remove,
+  deleteAll,
+  questions
+};
